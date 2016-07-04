85c985c0699db0a96992662c1698c145b16792d9
waday1 <g1527079@st.t-kougei.ac.jp>
2016-07-04 17:40:35 +0900

    Update default.js

--- default.js
+++ default.js
@@ -16,9 +16,7 @@ var counter = separate_time(now)
 document.getElementById('countdown').textContent=
 counter[5]+'年'+counter[4]+'月'+counter[3]+'日'+counter[2]+'時'+counter[1]+'分'+counter[0]+'秒';
 
-document.getElementById('form').select.onchange=function(){
-  location.href = document.getElementById('form').select.value
-}
+
 refresh();
 }
 
@@ -27,7 +25,24 @@ var refresh =function()
   setTimeout(update,1000);
 }
 
+function getFileName(){
+  return window.location.href.split('/').pop();
+}
+var filename = getFileName();
+var opt;
+if(filename === 'other.html')
+{
+  opt = document.querySelector('option[value="other.html"]');
+}
+else
+{
+ opt = document.querySelector('option[value="index.html"]');
+}
+opt.selected=true;
 
+document.getElementById('form').select.onchange=function(){
+  location.href = document.getElementById('form').select.value
+}
 
 update();
 
