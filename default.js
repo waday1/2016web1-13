c5db9c7f45ec943cb661227f2ce8a10257a4c5aa
waday1 <g1527079@st.t-kougei.ac.jp>
2016-07-04 17:27:46 +0900

    Update default.js

--- default.js
+++ default.js
@@ -8,9 +8,19 @@ var separate_time =function(time)
   var year = time.getFullYear();
   return[sec,min,hours,days,month,year]
 }
-
+var uodate =function()
+{
 var now = new Date();
 var counter = separate_time(now)
 
 document.getElementById('countdown').textContent=
 counter[5]+'年'+counter[4]+'月'+counter[3]+'日'+counter[2]+'時'+counter[1]+'分'+counter[0]+'秒';
+refresh();
+}
+
+var refresh =function()
+{
+  setTineout(uodate,1000);
+}
+
+uodate();
